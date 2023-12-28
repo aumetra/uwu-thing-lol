d4c131e90d8060dd46aae8719af5d9d4c0f04d3f
hille <hille@onsive.net>
2023-12-28 21:56:58 +0100

    * hot fixes

--- Backend/FixelPlut.Server/Services/FromPingPongService.cs
+++ Backend/FixelPlut.Server/Services/FromPingPongService.cs
@@ -39,13 +39,22 @@ public class FromPingPongService : ILoaderService
 
                 var rect1 = GenerateRectangle(tick.Player1X, tick.Player1Y, PlayerWidth, PlayerHeight, "ffff00");
                 var rect2 = GenerateRectangle(tick.Player2X, tick.Player2Y, PlayerWidth, PlayerHeight, "ffff00");
-                var ball = GenerateRectangle(tick.BallX, tick.BallY, BallWidth, BallHeight, "ffff00");
+                var ball = GenerateBall(tick.BallX, tick.BallY, BallWidth, BallHeight, "ffff00");
 
-                queueService.Clear();
+                queueService.SemaphoreSlim.Wait();
+                try
+                {
+                    queueService.Clear();
 
-                queueService.Add(rect1);
-                queueService.Add(rect2);
-                queueService.Add(ball);
+                    //queueService.Add(background);
+                    queueService.Add(rect1);
+                    queueService.Add(rect2);
+                    queueService.Add(ball);
+                }
+                finally
+                {
+                    queueService.SemaphoreSlim.Release();
+                }
             }
             catch (Exception ex)
             {
@@ -66,11 +75,75 @@ public class FromPingPongService : ILoaderService
 
     private IEnumerable<string> GenerateRectangle(int posX, int posY, int width, int height, string color)
     {
+        const string black = "000000";
+        const string white = "ffffff";
+
+        var movementOffset = 30;
+        var list = new List<string>();
+        var minY = posY - movementOffset;
+        var maxY = posY + height + movementOffset;
+        for (int x = posX; x < posX + width; x++)
+        {
+            for (int y = minY; y < maxY; y++)
+            {
+                string clr = color;
+
+                if (y < posY)
+                {
+                    clr = white;
+                }
+                else if (y > posY + height)
+                {
+                    clr = white;
+                }
+                else if (y == minY || y == maxY - 1)
+                {
+                    clr = black;
+                }
+                else if (x == posX || x == posX + width - 1)
+                {
+                    clr = black;
+                }
+                else if (x == (posX + width) / 2)
+                {
+                    clr = white;
+                }
+
+
+                list.Add(string.Format("PX {0} {1} {2}", x, y, clr));
+            }
+        }
+        Shuffle(ref list);
+        return list;
+    }
+
+    private IEnumerable<string> GenerateBall(int posX, int posY, int width, int height, string color)
+    {
+        var movementOffset = 30;
+
         var list = new List<string>();
         for (int x = posX; x < posX + width; x++)
         {
             for (int y = posY; y < posY + height; y++)
-                list.Add(string.Format("PX {0} {1} {2}", x, y, color));
+            {
+                string clr = "000000";
+
+                // check if coordinates are inside circle
+                if (Math.Pow(x - posX, 2) + Math.Pow(y - posY, 2) <= Math.Pow(width / 2, 2))
+                {
+                    clr = color;
+                }
+                else
+                {
+                    continue;
+                }
+
+                list.Add(string.Format("PX {0} {1} {2}", x, y, clr));
+                list.Add(string.Format("PX {0} {1} {2}", x + movementOffset, y + movementOffset, clr));
+                list.Add(string.Format("PX {0} {1} {2}", x + movementOffset, y - movementOffset, clr));
+                list.Add(string.Format("PX {0} {1} {2}", x - movementOffset, y - movementOffset, clr));
+                list.Add(string.Format("PX {0} {1} {2}", x - movementOffset, y + movementOffset, clr));
+            }
         }
         Shuffle(ref list);
         return list;
